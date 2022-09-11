9b24563e0b148514c991077543a50869c2aa1b73
ekat1983 <shumkovakatya@yandex.ru>
2022-09-11 23:31:15 +0300

    java

---
+++ src/main/java/ru/netology/service/CashbackHackerService.java
@@ -0,0 +1,13 @@
+package ru.netology.service;
+
+public class CashbackHackerService {
+    private final int boundary = 1000;
+
+    public int remain(int amount) {
+        if (amount == boundary) {
+            return 0;
+        }
+        return boundary - amount % boundary;
+    }
+
+}
---
+++ src/test/java/ru/netology/service/CashbackHackerServiceTest.java
@@ -0,0 +1,26 @@
+package ru.netology.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CashbackHackerServiceTest {
+
+    @Test
+    public void testRemain() {
+        CashbackHackerService service = new CashbackHackerService();
+        int actual = service.remain(1000);
+        int expected =0;
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemain2() {
+        CashbackHackerService service = new CashbackHackerService();
+        int actual = service.remain(900);
+        int expected = 100;
+
+        assertEquals(actual, expected);
+    }
+}
